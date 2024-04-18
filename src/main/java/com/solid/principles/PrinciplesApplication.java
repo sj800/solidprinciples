f30cfa5fb684de886778f04fdf660a6c0f3cdcd6
surajtinvio <102949975+surajtinvio@users.noreply.github.com>
2024-04-18 15:28:46 +0530

    first commit

---
+++ src/main/java/com/solid/principles/PrinciplesApplication.java
@@ -0,0 +1,13 @@
+package com.solid.principles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinciplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrinciplesApplication.class, args);
+	}
+
+}
